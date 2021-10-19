e5298f4d6f957587400d1f8c060e1547e15db757
rousemilano <routhberism@gmail.com>
2021-10-19 18:29:32 -0400

    add migrations and update settings, urls and models of manage_about

--- manage_about/models.py
+++ manage_about/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Skill(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Description')
+    name = models.CharField(max_length=50, verbose_name='Nombre')
     percentage = models.IntegerField(verbose_name='Porcentaje')
     
     class Meta:
@@ -15,3 +15,16 @@ class Skill(models.Model):
     def __unicode__(self):
         return self.name
         
+class Interests(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre')
+    icon = models.FileField(upload_to='about/img', verbose_name='Icon')
+
+    class Meta:
+        verbose_name = 'Interes'
+        verbose_name_plural = 'Intereses'
+    
+    def __str__(self):
+        return self.name
+    
+    def __unicode__(self):
+        return self.name
